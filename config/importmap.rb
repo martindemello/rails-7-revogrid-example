fb182c7524be1ad5f8a8bf9106ae9688c292171e
Martin DeMello <mdemello@google.com>
2022-03-06 21:31:40 -0800

    add revogrid to import map
    ```
    ./bin/importmap pin @revolist/revogrid
    ```

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@revolist/revogrid", to: "https://ga.jspm.io/npm:@revolist/revogrid@3.0.98/dist/index.js"
